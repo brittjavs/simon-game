e8d3e23432c478c1b664640088f7bdb21db243b4
Brittany Javalera <brittjavalera@gmail.com>
2020-11-17 23:00:09 -0800

    added playSound function that will be called in both nextSequence and on btn click. Created an animate press function that will change look of a button on click

--- game.js
+++ game.js
@@ -1,13 +1,32 @@
 let buttonColours = ["red", "blue", "green", "yellow"]
 
 let gamePattern = []
+let userClickedPattern = []
 function nextSequence(){
    let randomNumber = Math.floor(Math.random() * 4)
    let randomChosenColour = buttonColours[randomNumber]
    gamePattern.push(randomChosenColour)
    $("#" + randomChosenColour).animate({opacity: '0.5'}, 300);
    $("#" + randomChosenColour).animate({opacity: '1'});
-   let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+   playSound(randomChosenColour)
+}
+
+$(".btn").click(function(){
+    let userChosenColor = $(this).attr("id")
+    userClickedPattern.push(userChosenColor)
+    animatePress(userChosenColor)
+    playSound(userChosenColor)
+    
+})
+
+function playSound(name){
+    let audio = new Audio("sounds/" + name + ".mp3");
    audio.play();
 }
 
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed")
+    setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed")
+    }, 100)
+}
