e8dbc2e6e6b933be221b6cf455053f28c6c63004
Brittany Javalera <brittjavalera@gmail.com>
2020-11-18 19:39:31 -0800

    added check answer function, keypress event listener, added a call for checkAnswer on btn click, created level and started variables which are used in nextSequence

--- game.js
+++ game.js
@@ -2,20 +2,28 @@ let buttonColours = ["red", "blue", "green", "yellow"]
 
 let gamePattern = []
 let userClickedPattern = []
+let level = 0
+let started = false;
+
 function nextSequence(){
-   let randomNumber = Math.floor(Math.random() * 4)
-   let randomChosenColour = buttonColours[randomNumber]
-   gamePattern.push(randomChosenColour)
-   $("#" + randomChosenColour).animate({opacity: '0.5'}, 300);
-   $("#" + randomChosenColour).animate({opacity: '1'});
-   playSound(randomChosenColour)
-}
+    userClickedPattern = []
+    level++
+    $("#level-title").text("Level " + level)
+    let randomNumber = Math.floor(Math.random() * 4)
+    let randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+    $("#" + randomChosenColour).animate({opacity: '0.5'}, 300);
+    $("#" + randomChosenColour).animate({opacity: '1'});
+    playSound(randomChosenColour)
+
+   }
 
 $(".btn").click(function(){
     let userChosenColor = $(this).attr("id")
     userClickedPattern.push(userChosenColor)
     animatePress(userChosenColor)
     playSound(userChosenColor)
+    checkAnswer(userClickedPattern.length-1)
     
 })
 
@@ -29,4 +37,25 @@ function animatePress(currentColor){
     setTimeout(function(){
         $("#" + currentColor).removeClass("pressed")
     }, 100)
+}
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level " + level)
+        nextSequence()
+        started = true
+    }
+})
+
+function checkAnswer(currentLevel){
+ if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+     if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function(){
+            nextSequence()
+        }, 1000)
+     }
+ }
+ else{
+     console.log("wrong")
+ }
 }
